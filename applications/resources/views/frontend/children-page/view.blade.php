57debdf43202b155dfa5dcbcc8815f87a544ed7a
dessurya <fourline66@gmail.com>
2017-06-05 13:25:59 +0700

    children view footer

--- applications/resources/views/frontend/children-page/view.blade.php
+++ applications/resources/views/frontend/children-page/view.blade.php
@@ -37,6 +37,7 @@ body{
 	width: 100%;
 	height: 10vh;
 	top: -65px;
+	left: 0;
 	background-image: url('../amadeo/main-image/children-ft.png');
 }
 #footer .footer-wrapper .footer-content .fa-stack-1x{
