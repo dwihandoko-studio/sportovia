660e4756d8492ee37ec952ccfecbe0adc7376e64
fikrireza <fikrirezaa@gmail.com>
2017-09-19 16:45:06 +0700

    role aja

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -1,9 +1,13 @@
 <!--sidebar-menu-->
+@php
+  $role = auth()->guard('admin')->user()->role_id;
+@endphp
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
   <ul>
     <li class="{{ Route::is('dashboard') ? 'active' : '' }}">
       <a href="{{ route('dashboard') }}"><i class="icon icon-home"></i> <span>Dashboard</span></a>
     </li>
+    @if ($role == 1 || $role == 2 || $role == 4)
     <li class="submenu {{ Route::is('tentang*') ? 'active' : '' }}{{ Route::is('staff-jabatan*') ? 'active' : ''}}{{ Route::is('pegawai*') ? 'active' : ''}}">
       <a href="#"><i class="icon icon-asterisk"></i> <span>About</span></a>
       <ul>
@@ -12,12 +16,18 @@
         <li class="{{ Route::is('staff-jabatan*') ? 'active' : ''}}"><a href="{{ route('staff-jabatan.index') }}">Staff Position</a></li>
       </ul>
     </li>
+    @endif
+    @if ($role == 1 || $role == 2 || $role == 5)
     <li class="{{ Route::is('member*') ? 'active' : '' }}">
       <a href="{{ route('member.index') }}"><i class="icon icon-group"></i> <span>Member</span></a>
     </li>
+    @endif
+    @if ($role == 1 || $role == 2 || $role == 5)
     <li class="{{ Route::is('jadwal*') ? 'active' : '' }}">
       <a href="{{ route('jadwal.index') }}"><i class="icon icon-calendar"></i> <span>Schedule Course</span></a>
     </li>
+    @endif
+    @if ($role == 1 || $role == 2 || $role == 4)
     <li class="submenu {{ Route::is('kelasKategori*') ? 'active' : '' }}{{ Route::is('kelasKursus*') ? 'active' : '' }}{{ Route::is('kelasProgram*') ? 'active' : '' }}{{ Route::is('kelasRuang*') ? 'active' : '' }}{{ Route::is('kelasGaleri*') ? 'active' : '' }}{{ Route::is('allGaleri*') ? 'active' : '' }}">
       <a href="#"><i class="icon icon-magnet"></i> <span>Class</span></a>
       <ul>
@@ -28,6 +38,8 @@
         <li class="{{ Route::is('kelasRuang*') ? 'active' : '' }}"><a href="{{ route('kelasRuang.index') }}">Class Room</a></li>
       </ul>
     </li>
+    @endif
+    @if ($role == 1 || $role == 2 || $role == 4)
     <li class="submenu {{ Route::is('news*') ? 'active' : '' }}{{ Route::is('event*') ? 'active' : '' }}">
       <a href="#"><i class="icon icon-th-list"></i> <span>News & Events</span></a>
       <ul>
@@ -35,18 +47,28 @@
         <li class="{{ Route::is('event*') ? 'active' : '' }}"><a href="{{ route('event.index') }}">Event</a></li>
       </ul>
     </li>
+    @endif
+    @if ($role == 1 || $role == 2 || $role == 4)
     <li class="{{ Route::is('freetrial*') ? 'active' : '' }}">
       <a href="{{ route('freetrial.index') }}"><i class="icon-book"></i> <span>Free Trial Register</span></a>
     </li>
+    @endif
+    @if ($role == 1 || $role == 2 || $role == 4)
     <li class="{{ Route::is('ads*') ? 'active' : '' }}">
       <a href="{{ route('ads.index') }}"><i class="icon-picture"></i> <span>Ads Banner</span></a>
     </li>
+    @endif
+    @if ($role == 1 || $role == 2)
     <li class="{{ Route::is('socmed*') ? 'active' : '' }}">
       <a href="{{ route('socmed.index') }}"><i class="icon-facebook-sign"></i> <span>Social Media</span></a>
     </li>
+    @endif
+    @if ($role == 1 || $role == 2 || $role == 4)
     <li class="{{ Route::is('kontak*') ? 'active' : '' }}">
       <a href="{{ route('kontak.index') }}"><i class="icon icon-credit-card"></i> <span>Contact</span></a>
     </li>
+    @endif
+    @if ($role == 1 || $role == 2)
     <li class="{{ Route::is('facebook*') ? 'active' : '' }}">
       <a href="{{ route('facebook.index') }}"><i class="icon icon-facebook"></i> <span>Facebook App</span></a>
     </li>
@@ -56,6 +78,7 @@
     <li class="{{ Route::is('userAdmin*') ? 'active' : '' }}">
       <a href="{{ route('userAdmin.index') }}"><i class="icon icon-user"></i> <span>Users</span></a>
     </li>
+    @endif
   </ul>
 </div>
 <!--sidebar-menu-->
